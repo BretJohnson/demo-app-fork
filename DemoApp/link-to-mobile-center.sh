82614547bd2e232704682a80bb0388ccf9c848dc
Bret Johnson <bret.johnson@microsoft.com>
2016-12-05 11:48:58 -0800

    Linked to Mobile Center, for our fork

--- DemoApp/link-to-mobile-center.sh
+++ DemoApp/link-to-mobile-center.sh
@@ -3,5 +3,7 @@ echo 'Installing our npm packages...'
 npm install mobile-center-analytics --save
 npm install mobile-center-crashes --save
 echo 'Linking...'
+echo 'Android app secret should be: 13502f6c-53bc-496e-9517-f2454b4e03a5'
+echo 'iOS app secret should be: 96638174-9d13-4b2b-a110-1866d6bfa7dc'
 react-native link mobile-center-analytics
 react-native link mobile-center-crashes
